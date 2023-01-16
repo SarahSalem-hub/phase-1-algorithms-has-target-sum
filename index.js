dbd732473cba5e88ff39e740e12caeb4f43cdaf8
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-01-16 16:12:17 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      let sum = array[i] + array[j]
+      
+      if (sum === target){
+        return true
+      }
+     
+    }
+    
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here:
+  O(n^2): two loops iterates n times (array inputs)
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  loop over array (i item)
+     loop over array starts at the 2nd index (j item)
+       add i and j in sum variable
+        check if sum is equal to target 
+            return true
+  return false otherwise
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  - iterate over the array with 2 loops 
+    to get the sum and compare it to the target 
 */
 
 // You can run `node index.js` to view these console logs
